a3d014dc1cf125bff54a0333c4480a5c76b08339
Atabaev <blackpanda7@mail.ru>
2021-05-03 21:01:07 +0600

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {connect} from "react-redux";
+
+const App = ({state}) => {
+    console.log(state)
+    return (
+        <div>
+            <h1>hellofdhgdzsgz</h1>
+        </div>
+    );
+};
+const msp = (state) => {
+    return {state}
+}
+
+export default connect(msp)(App);
